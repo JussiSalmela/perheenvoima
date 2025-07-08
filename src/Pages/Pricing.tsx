f5ff3b8c0b25f6fdaeb29ce55d0815da66f0bc6e
jussi salmela <jusa.salmela@gmail.com>
2025-07-08 13:22:05 +0300

    päiväkodeille kohdennettu teksi lisätty

--- src/Pages/Pricing.tsx
+++ src/Pages/Pricing.tsx
@@ -15,23 +15,32 @@ export default function Pricing() {
             <h2>Palvelut ja hinnasto</h2>
             <h3>Ratkaisukeskeinen valmennus perheille</h3>
             <p>
-               lähivalmennus 60 min 85€<br/>
-               lähivalmennus 4 x 60 min 300€<br/>
-               <li>lisätunnit neljän kerran jälkeen 75€/h</li><br/>
+               lähivalmennus 60 min 85€<br />
+               lähivalmennus 4 x 60 min 300€<br />
+               <li>lisätunnit neljän kerran jälkeen 75€/h</li><br />
 
-               etävalmennus 60 min 75€<br/>
-               etävalmennus 4 x 60 min 250€ (suosituin)<br/>
-               <li>lisätunnit neljän kerran jälkeen 65€/h</li><br/>
+               etävalmennus 60 min 75€<br />
+               etävalmennus 4 x 60 min 250€ (suosituin)<br />
+               <li>lisätunnit neljän kerran jälkeen 65€/h</li><br />
             </p>
             <h3>Lastenhoitoapu itsemaksaville asiakkaille</h3>
             <p>
-               Arkisin 44 € / h sis. alv 25,5 %<br/>
-               Viikonloppuisin 50 € / h sis. alv 25,5 %<br/>
+               Arkisin 44 € / h sis. alv 25,5 %<br />
+               Viikonloppuisin 50 € / h sis. alv 25,5 %<br />
                Kilometrikorvaus tarvittaessa 0,50 € / km + alv
             </p>
-            <p style={{fontWeight: 'bold'}}>
+            <p style={{ fontWeight: 'bold' }}>
                Hinnat sisältävät alv 25,5%
             </p>
+            <h3>Päiväkodeille</h3>
+            <p>
+               Varhaiskasvatus ryhmän rauhallisen ilmapiirin luomiseen tarvitaan sujuvat siirtymät sekä struktuuri joka toimii just siinä ryhmässä. Annan työkaluja arjen sujuvoittamiseen käyttäen muun muassa erityis-pedagogisia menetelmiä.  Ryhmän hyvä ilmapiiri mahdollistaa lapselle laadukkaan varhaiskasvatuksen.
+               Autan kasvattajia ratkaisukeskeisellä toimintatavalla löytämään tavat jolla luodaan ryhmään rauhallinen ilmapiiri.
+            </p>
+            <p>
+               Ohjaan myös varhaiskasvatus ryhmässä tuettua leikkiä. Tuettu leikki voi tarkoittaa leikin havainnointia, lasten tasolle asettumista, leikin rikastuttamista sanoin tai elein, vertaissuhteiden tukemista ja leikkiympäristöstä huolehtimista sekä kuvien käyttö kommunikoinnin tukena leikissä.
+            </p>
+            <p>Ota yhteyttä niin räätälöidään sopiva pakettihinta.</p>
          </div>
       </PageComponent>
    )
